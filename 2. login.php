656f159852c4de9901445c2a0c6da91979445ad8
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-11-06 14:40:29 +0800

    Update 2. login.php

--- 2. login.php
+++ 2. login.php
@@ -1,6 +1,6 @@
 
 <?php 
-    echo $_GET[id]; <!--回傳訊息問ID-->
-    echo "<br>"; <!--回傳輸入ENTER-->
-    echo $_GET[pwd];<!--回傳問ENTER-->
+    echo $_GET[id]; //回傳訊息問ID
+    echo "<br>"; //回傳輸入ENTER
+    echo $_GET[pwd];//回傳問ENTER
 ?>
