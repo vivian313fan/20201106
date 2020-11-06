5e35d7383f969d350bb54c1bab4492c9a8d125d6
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-11-06 14:41:55 +0800

    Update 4. mydb.php

--- 4. mydb.php
+++ 4. mydb.php
@@ -1,11 +1,11 @@
 <？php
-    error_reporting（0）;  <!--不輸出任何錯誤-->
+    error_reporting（0）;  //不輸出任何錯誤
     
     $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;
-    如果（mysqli_connect_error（$ conn）） <!--傳回連結資料庫的錯誤碼。如果沒有錯誤發生則返回 0。-->
+    如果（mysqli_connect_error（$ conn）） //傳回連結資料庫的錯誤碼。如果沒有錯誤發生則返回 0。
         die（“資料庫連線錯誤”）;
 
-    mysqli_set_charset（$ conn，“ utf8”）; <!--設置字符集-->
+    mysqli_set_charset（$ conn，“ utf8”）; //設置字符集
     $ result = mysqli_query（ $ conn， “從用戶選擇*”）;
     while（$ row = mysqli_fetch_array（$ result））{
         echo  $ row [id];
