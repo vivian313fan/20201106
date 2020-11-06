f74c1d40229506f201b29c1f69833b9338765d91
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-11-06 13:39:53 +0800

    Update 4. mydb.php

--- 4. mydb.php
+++ 4. mydb.php
@@ -5,7 +5,7 @@
     如果（mysqli_connect_error（$ conn）） <!--傳回連結資料庫的錯誤碼。如果沒有錯誤發生則返回 0。-->
         die（“資料庫連線錯誤”）;
 
-    mysqli_set_charset（$ conn，“ utf8”）;
+    mysqli_set_charset（$ conn，“ utf8”）; <!--設置字符集-->
     $ result = mysqli_query（ $ conn， “從用戶選擇*”）;
     while（$ row = mysqli_fetch_array（$ result））{
         echo  $ row [id];
